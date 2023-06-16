1008278d7bccb1c97d46b1a1b33f7db7db69b981
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-16 09:08:36 -0700

    task 1 if loop

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description : checking if the number stored in n is positive or negative
+ *
+ *Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+	int n;
+
+	if (n > 0)
+	printf("n is positive: %d\n");
+	else if (n < 0)
+	printf("n is negative: %d\n");
+	else (n = 0)
+	printf("n equals 0: %d/n");
+	return (0);
+}
