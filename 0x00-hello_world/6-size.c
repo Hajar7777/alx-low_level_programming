b0575b53fa8d399f9486a14cc4787b25de0b04cf
HajarBenHfd <134013402+Hajar7777@users.noreply.github.com>
2023-06-16 04:17:38 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,19 @@
 /**
  *main - main entey paoint
  *
+ *Description: printing size of various types
+ *
  *description: counting size
  *
- *Return: Always 0 (Success)
+ *Return: Always 0(Success)
  */
 int main(void)
 }
 
 	long long int a;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof((char));
-	printf("Size of an int: %lu byte(s)\n", sizeof((int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof ((int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof((long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof((a));
 	printf("Size of a float: %lu byte(s)\n", sizeof((float));
