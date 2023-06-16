9e17a4f86f9bb61634c2da762baf725622528950
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-16 13:44:13 -0700

    sistar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	int n = 65;
-	int m = 97;
+	int n = 97;
+	int m = 65;
 
-	while (n <= 90)
+	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-	while (m <= 122)
+	while (m <= 90)
 	{
 		putchar(m);
 		m++;
