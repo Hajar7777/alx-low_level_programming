558e6e197dd7973297c685bbbbd8c1059276cd6b
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-16 13:17:57 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,13 +7,20 @@
 int main(void)
 {
 	int n;
+	int m;
 
 	n = 97;
+	m = 65;
 	while (n <= 122)
 	{
 		putchar(n);
 		n++;
 	}
-		putchar('\n');
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: always 0(Success)
+ */
+int main(void)
+{
+	int n = 65;
+	int m = 97;
+
+	while (n <= 90)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m  <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
