1b50ca3405aa26d33d73aa9f9147107344a731e9
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-20 16:31:26 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,33 @@ void times_tables(void)
 {
 	int a;
 	int b;
+	int c;
+	int d;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b < 10; b++)
 		{
-			if (a >= 9 && b >= 9)
-				break;
-			_putchar(a * b);
-			_putchar(32);
-			_putchar(44);
+			c = a * b;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				if (c < 10 && b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
 			_putchar('\n');
-		}
 	}
 }
