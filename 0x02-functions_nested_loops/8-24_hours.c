fb4eb64e27b24447c24b7668483e26896489b8a1
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-20 15:35:30 -0700

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (a >= 3 && b >= 4)
+					if (a >= 2 && b >= 4)
 						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
