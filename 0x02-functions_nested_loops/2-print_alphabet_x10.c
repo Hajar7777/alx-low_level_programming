976feb657643abf167c5422e8a4f11359ed424dd
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-20 11:58:47 -0700

    print alphas 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* print_alphabet_x10 - prints alphabets ten times
+* print_alphabet_x10 - Prints alphas 10 times.
 *
 * Return: Always 0(Success)
 */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int n;
 	int m;
@@ -15,7 +15,6 @@ int print_alphabet_x10(void)
 		{
 			_putchar(m);
 		}
-		_putchar(n);
 	}
 	_putchar('\n');
 }
