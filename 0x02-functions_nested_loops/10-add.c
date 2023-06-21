2494861cbdd255fe38f28048be51845c4f3d9ebd
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-20 19:13:49 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * add - Adds two integers and returning result.
+  *
+  * @a : the first integer
+  * @b : the second integer
+  *
+  * Return: integer value
+  */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,40 @@
 #include "main.h"
 /**
-  * times_tables - Prints the 9 times table starting from 0.
-  */
-void times_tables(void)
+* times_tables - Prints 9 times table, starting with 0.
+*
+* Description : printing times table of integer 9
+*
+* Return: Always 0(Success)
+*/
+void times_table(void)
 {
-	int a;
-	int b;
-	int c;
+	int a, b, c, d, e;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-				if (b == 0)
-				{
-					_putchar(c + 48);
-				}
-				if (c < 10 && b != 0)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
-					_putchar(c + 48);
-				}
-				else if (c >= 10)
-				{
-					_putchar(44);
-					_putchar(32);
-					_putchar((c / 10) + 48);
-					_putchar((c % 10) + 48);
-				}
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(e + '0');
+				_putchar(c + '0');
 			}
-			_putchar('\n');
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
 	}
 }
