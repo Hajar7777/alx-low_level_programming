09ba1f83ec11ba8774400f32a91dbf88497f069a
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-22 10:58:26 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "unistd.h"
+#include "main.h"
+/**
+* print_numbers - prints the numbers from 0 to 9.
+*
+* Return: Always 0(Success)
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
