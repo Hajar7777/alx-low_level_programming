67945ef052f431e174e8e1cf0c5dafff7c565e8d
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-22 11:37:25 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_most_numbers - prints numbers from 0 to 9.
+*
+* return: returns integers
+*/
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	if (a == 2 || a == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(a + '0');
+	}
+	}
+	_putchar(a + '0');
+}
