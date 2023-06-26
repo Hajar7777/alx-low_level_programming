70192241bbaa3e6ba13425bbc69761a9581002de
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-26 14:57:39 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * reset_to_98 - updates the value it points to to 98
+  * @*n : pointer
+  * Return: empty
+  */
+void reset_to_98(int *n)
+{
+	int a = 98;
+	int *n = &a;
+
+	_putchar(b);
+}
