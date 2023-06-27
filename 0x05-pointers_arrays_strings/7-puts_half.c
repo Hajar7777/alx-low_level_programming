b17c9dcb4a992111db07d10ecf2eaac3750f1885
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-26 17:13:18 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * puts_half - prints half the string followed by a new line
+  *
+  * @str : string parametre
+  *
+  * Return : empty
+  */
+void puts_half(char *str)
+{
+	int w;
+
+	for (w = 0; str[w] != '\0'; w++)
+		;
+	w++;
+	for (w / 2; str[w] != '\0'; w++)
+	{
+		_putchar(str[w]);
+	}
+	_putchar('\n');
+}
