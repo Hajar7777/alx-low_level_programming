106b32d3198389a0fd4779c2ba54c5136745a1dc
hajar hafdi <hajarbenlakbir@outlook.com>
2023-06-29 16:05:02 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
+
 /**
-* _strncpy - copy a string
-*
-* @dest : pointer to destination
-* @src : pointer to source
-* @n : input value
-*
-* Return : dest
-*/
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
