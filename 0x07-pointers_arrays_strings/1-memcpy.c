a631c90227419ea0d68b8b087550ca133d438e3d
hajar hafdi <hajarbenlakbir@outlook.com>
2023-07-03 11:04:19 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _memcpy - function that copies memory area
+*
+* @dest : destination pointer
+*
+* @src : source pointer
+*
+* @n : unsigned int
+*
+* Return: pointer dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int l;
+
+	for (l = 0; l < n; l++)
+	{
+		src[l] = dest[l];
+	}
+	return (dest);
+}
