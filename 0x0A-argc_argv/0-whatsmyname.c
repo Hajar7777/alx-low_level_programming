0c496beabf39500be832ed9bdda1abf30bb92cf4
hajar hafdi <hajarbenlakbir@outlook.com>
2023-07-07 07:25:34 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* @argc : int
+* @argv : list
+*
+* Return: always 0(Success)
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
