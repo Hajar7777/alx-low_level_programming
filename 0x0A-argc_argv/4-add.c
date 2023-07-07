92ac7ffc7b249507c41ffdb2c8d537bee313c674
hajar hafdi <hajarbenlakbir@outlook.com>
2023-07-07 08:40:35 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+*
+* @argc : int
+* @argv: pointer
+*
+* Return: always 0(Success)
+*/
+int main(int argc, char **argv)
+{
+	int m = 0;
+	char *c;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c < '9')
+				return (printf("Error\n"), 1);
+		m += atoi(argv[argc]);
+	}
+	printf("%d\n", m);
+	return (0);
+}
