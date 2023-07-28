a3e313f0b4d5c06874e97bf811adbb45a723c5b7
Hajar <hajarbenlakbir@outlook.com>
2023-07-28 06:13:20 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,12 @@
+section     .text
 global   main
-	 extern    printf
+extern    printf
+
 main:
-	  mov   edi, format
-	  xor   eax, eax
-	  call  printf
-	  mov   eax, 0
+	  mov          edi, format
+	  xor          eax, eax
+	  call         printf
+	  mov          eax, 0
 	  ret
+section      .data
 format: db `Hello, Holberton\n`,0
