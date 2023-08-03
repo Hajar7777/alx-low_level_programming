6416619f9975f05442073cc56e6d46b2fa12d8bf
Hajar <hajarbenlakbir@outlook.com>
2023-08-03 04:57:52 -0700

    0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include "main.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+* _putchar - WriTes CHAR c tO stdout
+*
+* @c: CHAR to BE  printED
+*
+* Return: SUcceSs 1
+* ERROR -1 and errno is TO BE set apprOpriately
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
